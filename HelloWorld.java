fb8106129dba2010181ea4d4c82942019a81477d
Shreyanka-R <Shreyankarenukaswamy@gmail.com>
2021-02-16 12:42:22 +0530

    add helloworld program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+	public static void main(String[] s)
+	{
+		System.out.println("Hello World");
+		for(int i=0; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+	
+	}
+}
